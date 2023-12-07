341230037dffbfacc990e18679924a708c71d116
Alex Emelyanov <holyketzer@gmail.com>
2023-12-07 11:25:53 +0300

    Fix puma config

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,21 +1,14 @@
-# config/puma.rb
+environment "production"
 threads 8, 8
 workers 1
 
+cwd = "/var/www/your_stock_index/current"
+
 on_worker_boot do
   require "active_record"
-  # cwd = File.dirname(__FILE__) + "/../.."
-  cwd = File.absolute_path(File.join(`pwd`.strip(), "../.."))
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")["production"])
 end
 
 bind "unix://#{cwd}/tmp/sockets/puma_web.sock"
-environment "production"
 pidfile "#{cwd}/tmp/pids/server.pid"
-
-# state_path "#{cwd}/tmp/sockets/puma.state"
-
-rackup "#{cwd}/config.ru"
-
-activate_control_app
